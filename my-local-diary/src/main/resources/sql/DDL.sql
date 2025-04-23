be4a97f33ba088e363d4e4b853a9cb167ca66415
Hancoal <hshgood035@naver.com>
2025-04-23 14:05:15 +0900

    feat ddl구문 완성본(기존 구문에 신고,알림, 팔로우,스탬프 추가)

--- my-local-diary/src/main/resources/sql/DDL.sql
+++ my-local-diary/src/main/resources/sql/DDL.sql
@@ -1,3 +1,10 @@
+DROP TABLE IF EXISTS member_stamp;
+DROP TABLE IF EXISTS follow;
+DROP TABLE IF EXISTS notification;
+DROP TABLE IF EXISTS suspension;
+DROP TABLE IF EXISTS report;
+DROP TABLE IF EXISTS report_reason;
+DROP TABLE IF EXISTS stamp;
 DROP TABLE IF EXISTS photo;
 DROP TABLE IF EXISTS place;
 DROP TABLE IF EXISTS likes;
@@ -79,3 +86,65 @@ CREATE TABLE photo (
   post_id INT,
   FOREIGN KEY (post_id) REFERENCES post(id)
 );
+
+CREATE TABLE stamp (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT
+);
+
+CREATE TABLE member_stamp (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  achieved_date VARCHAR(255),
+  member_id INT NOT NULL,
+  stamp_id INT NOT NULL,
+  FOREIGN KEY (member_id) REFERENCES member(id),
+  FOREIGN KEY (stamp_id) REFERENCES stamp(id)
+);
+
+CREATE TABLE follow (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  following_member_id INT NOT NULL,
+  follow_target_member_id INT NOT NULL,
+  FOREIGN KEY (following_member_id) REFERENCES member(id),
+  FOREIGN KEY (follow_target_member_id) REFERENCES member(id)
+);
+
+CREATE TABLE notification (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  type VARCHAR(255),
+  target_id INT,
+  content TEXT,
+  is_read BOOLEAN,
+  created_at VARCHAR(255),
+  recieving_member_id INT NOT NULL,
+  FOREIGN KEY (recieving_member_id) REFERENCES member(id)
+);
+
+CREATE TABLE suspension (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  suspension_start_date VARCHAR(255),
+  suspension_end_date VARCHAR(255),
+  type VARCHAR(255),
+  member_id INT NOT NULL,
+  FOREIGN KEY (member_id) REFERENCES member(id)
+);
+
+CREATE TABLE report_reason (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   reason TEXT
+);
+
+CREATE TABLE report (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    created_at VARCHAR(255),
+    report_type VARCHAR(255),
+    reported_id INT,
+    content TEXT,
+    status VARCHAR(255),
+    member_id INT NOT NULL,
+    report_reason_id INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (report_reason_id) REFERENCES report_reason(id)
+);
+
