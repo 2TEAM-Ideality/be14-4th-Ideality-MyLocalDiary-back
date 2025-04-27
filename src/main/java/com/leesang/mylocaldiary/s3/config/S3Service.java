99919f27b19566d8d99ee1170333753dc200e885
minsun24 <jms49526@gmail.com>
2025-04-28 01:42:32 +0900

    Feature: S3 환경설정

---
+++ src/main/java/com/leesang/mylocaldiary/s3/config/S3Service.java
@@ -0,0 +1,42 @@
+package com.leesang.mylocaldiary.s3.config;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    private final String bucketName = "my-local-diary-prod"; // 버킷 이름
+
+    // 테스트용
+    public String uploadFile(MultipartFile file, String folderName) throws IOException {
+        // 파일 이름 랜덤하게 생성
+        String fileName = folderName + "/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(file.getContentType())
+                .build();
+
+        s3Client.putObject(
+                putObjectRequest,
+                RequestBody.fromInputStream(file.getInputStream(), file.getSize())
+        );
+
+        return fileName; // 저장된 Key (경로/파일명) 반환
+    }
+
+
+}
