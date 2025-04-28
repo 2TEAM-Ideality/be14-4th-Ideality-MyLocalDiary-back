ebcb6e6f9a6ff7cbc2fff263e145f47530abb32c
rainyday1367 <rainyday1367@naver.com>
2025-04-29 01:09:15 +0900

    feat smtp 설정 및 redis 메일 관련 메소드 설정 완료

---
+++ src/main/java/com/leesang/mylocaldiary/email/service/EmailSendServiceImpl.java
@@ -0,0 +1,9 @@
+package com.leesang.mylocaldiary.email.service;
+
+import com.leesang.mylocaldiary.redis.util.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSendServiceImpl implements EmailSendService {
+}
