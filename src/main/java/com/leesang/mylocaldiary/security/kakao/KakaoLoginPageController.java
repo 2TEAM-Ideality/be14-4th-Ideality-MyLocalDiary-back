02bee2d6e597978e5cc8e18e6bb5e58730424075
mins00kwon <mskwon0918@gmail.com>
2025-04-28 20:15:49 +0900

    feat:카카오 로그인 - 카카오에서 정보 가져오기 성공

---
+++ src/main/java/com/leesang/mylocaldiary/security/kakao/KakaoLoginPageController.java
@@ -0,0 +1,26 @@
+package com.leesang.mylocaldiary.security.kakao;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class KakaoLoginPageController {
+
+    @Value("${kakao.client_id}")
+    private String client_id;
+
+    @Value("${kakao.redirect_uri}")
+    private String redirect_uri;
+
+    @GetMapping("/kakao")
+    public String kakaoLoginRedirect() {
+        String kakaoAuthUrl = "https://kauth.kakao.com/oauth/authorize"
+                + "?response_type=code"
+                + "&client_id=" + client_id
+                + "&redirect_uri=" + redirect_uri;
+        return "redirect:" + kakaoAuthUrl;
+    }
+}
