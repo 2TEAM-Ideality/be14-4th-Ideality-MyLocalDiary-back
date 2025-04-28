b835bd6a4945f0b27b3accb2f174861c1526b9fd
Hancoal <hshgood035@naver.com>
2025-04-28 18:14:58 +0900

    no message

--- src/main/java/com/leesang/mylocaldiary/member/repository/MemberRepository.java
+++ src/main/java/com/leesang/mylocaldiary/member/repository/MemberRepository.java
@@ -9,10 +9,3 @@ public interface MemberRepository extends JpaRepository<MemberEntity, Integer> {
 
     Optional<Object> findByLoginId(String loginId);
 }
-
-
-//import com.leesang.mylocaldiary.member.entity.Member;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface MemberRepository extends JpaRepository<Member, Long> {
-//}
